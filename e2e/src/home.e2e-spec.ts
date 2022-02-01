8caf3595e5016b29784f8acdb8ab2c5eef44a796
Luiz Vieira <luiz22_sp@hotmail.com>
2022-02-01 07:24:29 -0300

    02 interagindo com elementos

--- e2e/src/home.e2e-spec.ts
+++ e2e/src/home.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { browser } from 'protractor';
+import { browser, element, by, protractor } from 'protractor';
 
 describe('Home page', () => {
 
@@ -6,6 +6,20 @@ describe('Home page', () => {
     await browser.get(`${browser.baseUrl}/#/user/flavio`);
     const title = await browser.getTitle();
     expect(title).toEqual('Timeline');
+  });
+
+  it('Should display a list of photos', async () => {
+    const list = element.all(by.css('.photo'));
+    const photoListSize = await list.count();
+    expect(photoListSize).toBeGreaterThan(0);
+  });
+
+  it('Should navigate to photo detail when photo navigation is triggered', async () => {
+    await browser.get(`${browser.baseUrl}/#/user/flavio`);
+    const firstElement = element.all(by.css('.photo')).first();
+    await firstElement.sendKeys(protractor.Key.ENTER);
+    const title = await browser.getTitle();
+    expect(title).toBe('Photo detail');
   });
 
 });
